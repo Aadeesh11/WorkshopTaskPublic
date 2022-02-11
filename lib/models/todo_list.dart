bf05255f7fb4692ad5ecd3d5478695eb63f28aae
kb15bhu <99141096+kb15bhu@users.noreply.github.com>
2022-02-11 16:53:11 +0530

    Update todo_list.dart

--- lib/models/todo_list.dart
+++ lib/models/todo_list.dart
@@ -2,16 +2,20 @@ import 'package:workshop_task/models/todo.dart';
 
 class TodoList {
   final List<Todo> _allTodos = [];
+  final List<Todo> _allTodos = <Todo>[];
 
   List<Todo> allTodos() {
-    //TODO:Add logic to complete
+    //to complete
+    return _allTodos;
   }
 
   void addTodo(Todo todo) {
-    //TODO:Add logic to add a todo
+    //to add a todo
+    _allTodos.add(todo);
   }
 
   void deleteTodo(Todo todo) {
-    //TODO:Add logic to delete todo
+    //to delete todo
+    _allTodos.remove(todo);
   }
 }

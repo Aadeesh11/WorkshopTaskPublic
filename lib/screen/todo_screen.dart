e72240e47edb773ccc4e37e78eb4480e699d0e8d
Adrito-M <adrito.mukherjee.cse21@itbhu.ac.in>
2022-02-13 21:34:27 +0530

    passes all tests

--- lib/screen/todo_screen.dart
+++ lib/screen/todo_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:workshop_task/models/todo_list.dart';
 
+import 'package:workshop_task/widgets/add_todo_dialogue.dart';
+import 'package:workshop_task/widgets/todo_list_item.dart';
+
 class TodoScreen extends StatefulWidget {
   const TodoScreen({Key key}) : super(key: key);
 
@@ -9,7 +12,20 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
+  // final TextEditingController controller1 = TextEditingController();
+  // final TextEditingController controller2 = TextEditingController();
+
   TodoList todoList = TodoList();
+
+  Widget wigbody = Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: const [
+      Center(
+        child: Text("No Todos Added"),
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +33,59 @@ class _TodoScreenState extends State<TodoScreen> {
       appBar: AppBar(
         title: const Text("Your Todos"),
       ),
+      // ignore: todo
       //TODO: Add todo button with this icon => "+".
-      floatingActionButton: const FloatingActionButton(),
-      body: //TODO: Add list view displaying all todo.
-          Container(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context2) {
+              return const AddTodoDialogue();
+            },
+          ).then((value) {
+            setState(() {
+              todoList.addTodo(value);
+            });
+          });
+        },
+      ),
+      body: todoList.allTodos().isNotEmpty
+          ? ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: todoList.allTodos().length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                    onDoubleTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context_) {
+                            return AlertDialog(
+                              content: const Text(
+                                  "Are you sure you want to delete this Todo?"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        todoList.deleteTodo(
+                                            todoList.allTodos()[index]);
+                                        Navigator.of(context_).pop();
+                                      });
+                                    },
+                                    child: const Text("Yes")),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context_).pop();
+                                    },
+                                    child: const Text("No"))
+                              ],
+                            );
+                          });
+                    },
+                    child: TodoListItem(
+                        todo: todoList.allTodos()[index], index: index));
+              })
+          : const Center(child: Text("No Todo's added")),
     );
   }
 }
